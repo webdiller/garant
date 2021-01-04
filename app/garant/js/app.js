dca30c7a87e24d75a6a398fee2dd6e7a690d8ecb
eugene <eugenefromrus@gmail.com>
2021-01-04 23:38:21 +1000

    Готовый слайдер без стрелок

--- app/garant/js/app.js
+++ app/garant/js/app.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const headerModule = (function () {
+document.addEventListener('DOMContentLoaded', function(){
+	const headerModule = (function () {
     try {
       let menu_phone = document.getElementById('menu_phone');
       let menu_dropdown = document.getElementById('menu_dropdown');
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
         menu_dropdown.classList.toggle('active');
       });
     } catch (error) {}
-
+  
     try {
       let menu_call = document.getElementById('menu_call');
       let menu_bar = document.getElementById('menu_bar');
@@ -54,47 +54,13 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     } catch (error) {}
   })();
-
+  
   const lightBoxModule = (function () {
     try {
+      
     } catch (error) {}
   })();
-  const sliderModule = (function () {
-    try {
-      const mainImgWrapper = document.querySelector('#slider .slider__main-img-wrapper');
-      const mainImg = document.querySelector('#slider .slider__main-img');
-      const thumbImagesWrapper = document.querySelectorAll('#slider .slider__thumb-wrapper');
-      const thumbImages = document.querySelectorAll('#slider .slider__thumb-wrapper img');
-      const imgWrapper1 = document.querySelector('#slider .slider__thumb-wrapper:nth-child(1)');
-      const imgWrapper2 = document.querySelector('#slider .slider__thumb-wrapper:nth-child(2)');
-      const imgWrapper3 = document.querySelector('#slider .slider__thumb-wrapper:nth-child(3)');
-      const img1 = document.querySelector('#slider .slider__thumb-wrapper:nth-child(1) img');
-      const img2 = document.querySelector('#slider .slider__thumb-wrapper:nth-child(2) img');
-      const img3 = document.querySelector('#slider .slider__thumb-wrapper:nth-child(3) img');
-      const arrowLeft = document.querySelector('#slider .slider__arrow-left');
-      const arrowRight = document.querySelector('#slider .slider__arrow-right');
-
-      thumbImagesWrapper.forEach((item) => {
-        item.addEventListener('click', function (e) {
-          mainImgWrapper.classList.add('active');
-          thumbImagesWrapper.forEach((item) => item.classList.remove('current'));
-          e.target.parentElement.classList.add('active', 'current');
-          setTimeout(() => {
-            let srcMain = mainImg.src;
-            let srcChild = e.target.src;
-            mainImg.src = srcChild;
-            e.target.src = srcMain;
-          }, 300);
-          setTimeout(() => {
-            mainImgWrapper.classList.remove('active');
-            e.target.parentElement.classList.remove('active');
-          }, 300);
-        });
-      });
-
-    } catch (error) {}
-  })();
-
+  
   // console.log(
   //   'Разработка: webdiller.ru - Евгений Бутков - tg:webdillerru - +79996159789 - eugenefromrus@gmail.com'
   // );
